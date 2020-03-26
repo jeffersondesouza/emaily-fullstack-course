f24f88703ee407b12a85e3169c1480d234ef21f1
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2020-03-26 08:40:53 -0300

    disable log

--- index.js
+++ index.js
@@ -9,5 +9,6 @@ app.get('/', (req, res) => {
 const { PORT } = process.env;
 
 app.listen(PORT || 5000, () => {
+  // eslint-disable-next-line no-console
   console.log(`App runing at: http://localhost:${PORT || 5000}/`);
 });
