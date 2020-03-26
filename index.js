613247b18377fb6622e46061435ba8ef4340663d
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2020-03-26 10:16:47 -0300

    instal heroku

--- index.js
+++ index.js
@@ -12,3 +12,9 @@ app.listen(PORT || 5000, () => {
   // eslint-disable-next-line no-console
   console.log(`App runing at: http://localhost:${PORT || 5000}/`);
 });
+
+
+/* 
+https://morning-chamber-92877.herokuapp.com/ 
+https://git.heroku.com/morning-chamber-92877.git
+ */
