398d89cb6394229e78268ecd2c6d5813a4c456e2
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2020-03-28 07:41:15 -0300

    auth

--- index.js
+++ index.js
@@ -1,34 +1,11 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
-const app = express();
-
-const keys = require('./config/keys');
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientId,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback',
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log({ accessToken, refreshToken, profile, done });
-    }
-  )
-);
-
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email'],
-  })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
+require('./services/passport');
 
 const { PORT } = process.env;
+const app = express();
+
+require('./routes/auth')(app);
+
 app.listen(PORT || 5000, () => {
   // eslint-disable-next-line no-console
   console.log(`App runing at: http://localhost:${PORT || 5000}/`);
