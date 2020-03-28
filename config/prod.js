84642da234e21493d0039f070589e4e6e7932daf
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2020-03-28 17:19:54 -0300

    env on local

---
+++ config/prod.js
@@ -0,0 +1,16 @@
+module.exports = {
+  googleClientId: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URL,
+  cookieKey: process.env.COOKIE_KEY,
+};
+
+/* 
+
+  googleClientId:
+    '119552001929-3mhg37qpga3qs8859rq7jko4bdpkm1et.apps.googleusercontent.com',
+  googleClientSecret: 'efYftcK5VVIV-czZGmm9BtXh',
+  mongoURI:
+    'mongodb+srv://duppoe:duppoe@cluster0-giujg.mongodb.net/test?retryWrites=true&w=majority',
+  cookieKey: 'qwer',  
+  */
