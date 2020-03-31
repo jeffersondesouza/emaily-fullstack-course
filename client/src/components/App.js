bec00285dcdd71350f9b006601be83c6d796c512
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2020-03-31 07:15:39 -0300

    refacg

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,19 +3,7 @@ import React, { useEffect } from 'react';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="/auth/google"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Login with Google
-        </a>
-      </header>
+      <h1>Welcome to Emaily!</h1>
     </div>
   );
 }
