1f0362cf741f02ff196ab1e09f6bf5384f66386d
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2020-04-03 07:23:17 -0300

    config serve dploy client

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,7 @@
-if (process.env.NODE_ENV === 'production') {
+/* eslint-disable global-require */
+const envVariables = require('../services/envVariables');
+
+if (envVariables.isProduction()) {
   module.exports = require('./prod');
 } else {
   module.exports = require('./dev');
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const bodyParser = require('body-parser');
+const path = require('path');
 const cookieSession = require('cookie-session');
 // const cors = require('cors');
 /* import crypto from 'crypto'; */
@@ -10,6 +11,7 @@ const requireLoginMd = require('./middlewares/requireLoginMd');
 
 require('./models/User');
 require('./services/passport');
+const envVariables = require('./services/envVariables');
 
 mongoose.connect(keys.mongoURI, {
   useNewUrlParser: true,
@@ -44,6 +46,19 @@ app.get('/casos', (req, res) => {
 
 require('./routes/auth')(app);
 require('./routes/billing')(app, requireLoginMd);
+
+/* CLIENT Routes */
+if (envVariables.isProduction()) {
+  // Express will serve up production assests
+  // like main.js or main.css, they are at client ./build
+  app.use(express.static('client/build'));
+
+  // Express will serve up  the index.html file
+  // if it doesn't recognize the route
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(PORT || 5000, () => {
   // eslint-disable-next-line no-console
