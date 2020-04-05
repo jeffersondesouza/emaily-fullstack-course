2dbfa90b06bf06c8b4b7f8a9f4fc87122be552e7
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2020-04-05 11:20:25 -0300

    send ok

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -9,14 +9,13 @@ export const fetchUser = () => async dispatch => {
 
 export const handleToken = token => async dispatch => {
   const res = await axios.post('/api/stripe', token);
-  console.log('res:', res);
   const { data } = res;
   dispatch({ type: FETCH_USER, payload: data });
 };
 
 export const submitSurvey = survey => async dispatch => {
-  dispatch({ type: SUBMIT_SURVEY, payload: survey });
-
   const res = await axios.post('/api/surveys', survey);
-  console.log('res:', res);
+
+  const { data } = res;
+  dispatch({ type: FETCH_USER, payload: data });
 };
